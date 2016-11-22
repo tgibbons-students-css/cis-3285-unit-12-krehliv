89ba882eedc872197a0d38ea17b30e554594eaa7
Krehl Stringer <kstringer@css.edu>
2016-11-22 11:04:54 -0600

    Committing

--- Console/Program.cs
+++ Console/Program.cs
@@ -16,9 +16,18 @@ namespace ConsoleSample
             EntityRepository entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
-            myEntity.Name = "Tom";
+            myEntity.Name = "Krehl";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
+
+            IEntityRepository<User> userRepository = new UserRepository();
+            User myUser = userRepository.GetByID(Guid.NewGuid());
+
+            myUser.Name = "Krehl";
+            myUser.EmailAddress = "kstringer@css.edu";
+            Console.WriteLine("New entity ID: " + myUser.ID.ToString());
+            Console.WriteLine("New entity Name: " + myUser.Name);
+            Console.WriteLine("New entity EmailAddress: " + myUser.EmailAddress);
 
         }
     }
